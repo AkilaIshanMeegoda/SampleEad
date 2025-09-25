a909b92b37edeaa4d8d1c576164a75c0df2ceec8
Akila Ishan Meegoda <akilaishanmeegoda@gmail.com>
2025-09-25 23:21:49 +0530

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,7 +32,7 @@ export default function Login() {
           placeholder="Password"
           type="password"
         />
-        <button>Login</button>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
       </form>
       {err && <p style={{ color: "red" }}>{err}</p>}
     </div>
